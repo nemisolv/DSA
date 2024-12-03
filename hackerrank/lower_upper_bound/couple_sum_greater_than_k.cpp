7c27cb559dd91f9290f5c191621d5adff8fac75a
nemisolv <nemisolv@gmail.com>
2024-12-04 04:07:52 +0700

    lower+ upper bound

---
+++ hackerrank/lower_upper_bound/couple_sum_greater_than_k.cpp
@@ -0,0 +1,29 @@
+/** Cặp số có tổng bằng K
+ *  Cho mảng a gồm n phần tử và số nguyên K. Hãy đếm số cặp số (a[i], a[j]) (i!=j) sao cho a[i] + a[j] > k.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int &x : a)
+    {
+        cin >> x;
+    }
+    int k;
+    cin >> k;
+    int cnt = 0;
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
+    {
+        int x = k - a[i];
+        auto it = upper_bound(a + i + 1, a + n, x);
+        cnt += a+n - it;
+
+    }
+    cout << cnt;
+}
